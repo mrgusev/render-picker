2eff0f3264ef3ea3a7eacdbbc4cdc74e9f218475
Kirill Gusev <mr.gusev.k@gmail.com>
2014-04-29 20:52:18 +0400

    Inserted picker into a site.

---
+++ Primers & Adhesives_files/cufon_styles.js
@@ -0,0 +1,25 @@
+
+Cufon.replace('h1', { fontFamily: 'HelveNeuBolExt'});
+Cufon.replace('.gold-box h3', { fontFamily: 'Humanist 521 BT'});
+Cufon.replace('#slideshow h2', { fontFamily: 'Humanist 521 BT'});
+Cufon.replace('.pod h2', { fontFamily: 'Humanist 521 BT'});
+Cufon.replace('.category-pod h2', { fontFamily: 'Humanist 521 BT'});
+Cufon.replace('.item-pod h2', { fontFamily: 'Humanist 521 BT'});
+Cufon.replace('#buy-stonefix-cta h2', { fontFamily: 'Humanist 521 BT'});
+
+//Cufon.replace('#intropanel p.biocote');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
